9c98892692d7784e3eee6fde815e4d21a7e9422d
ego <108820480+disbuted@users.noreply.github.com>
2025-02-04 21:00:31 +0000

    Update xor.h

--- protection/encryption/xor.h
+++ protection/encryption/xor.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <string>
 #include <utility>
 
@@ -155,3 +154,81 @@ constexpr ALWAYS_INLINE auto _xor_(char32_t const (&str)[_length])
 	return _xor_U32<_length>(str);
 }
 //---------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////
+template <int X> struct EnsureCompileTime {
+	enum : int {
+		Value = X
+	};
+};
+////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////
+//Use Compile-Time as seed
+#define Seed ((__TIME__[7] - '0') * 1  + (__TIME__[6] - '0') * 10  + \
+              (__TIME__[4] - '0') * 60   + (__TIME__[3] - '0') * 600 + \
+              (__TIME__[1] - '0') * 3600 + (__TIME__[0] - '0') * 36000)
+////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////
+constexpr int LinearCongruentGenerator(int Rounds) {
+	return 1013904223 + 1664525 * ((Rounds > 0) ? LinearCongruentGenerator(Rounds - 1) : Seed & 0xFFFFFFFF);
+}
+#define Random() EnsureCompileTime<LinearCongruentGenerator(10)>::Value //10 Rounds
+#define RandomNumber(Min, Max) (Min + (Random() % (Max - Min + 1)))
+////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////
+template <int... Pack> struct IndexList {};
+////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////
+template <typename IndexList, int Right> struct Append;
+template <int... Left, int Right> struct Append<IndexList<Left...>, Right> {
+	typedef IndexList<Left..., Right> Result;
+};
+////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////
+template <int N> struct ConstructIndexList {
+	typedef typename Append<typename ConstructIndexList<N - 1>::Result, N - 1>::Result Result;
+};
+template <> struct ConstructIndexList<0> {
+	typedef IndexList<> Result;
+};
+////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////
+const char XORKEY = static_cast<char>(RandomNumber(0, 0xFF));
+
+constexpr char EncryptCharacter(const char Character, int Index) {
+	return Character ^ (XORKEY + Index);
+}
+
+template <typename IndexList> class C_xor_ing;
+template <int... Index> class C_xor_ing<IndexList<Index...> > {
+private:
+	char Value[sizeof...(Index) + 1];
+public:
+	constexpr C_xor_ing(const char* const String)
+		: Value{ EncryptCharacter(String[Index], Index)... } {}
+
+	char* decrypt() {
+		for (int t = 0; t < sizeof...(Index); t++) {
+			Value[t] = Value[t] ^ (XORKEY + t);
+		}
+		Value[sizeof...(Index)] = '\0';
+		return Value;
+	}
+
+	char* get() {
+		return Value;
+	}
+};
+#define XorS(X, String) C_xor_ing<ConstructIndexList<sizeof(String)-1>::Result> X(String)
